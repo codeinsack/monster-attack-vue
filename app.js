c43d62faae9fb3b9f3e0624b607ef69bb4747a82
codeinsack <codeinsack@gmail.com>
2020-02-01 10:35:48 +0300

    add logs

--- app.js
+++ app.js
@@ -4,22 +4,34 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame() {
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+      this.turns = []
     },
     attack() {
-      this.monsterHealth -= this.calculateDamage(3, 10)
+      const damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster for ${damage}`,
+      })
       if (this.checkWin()) {
         return
       }
       this.monsterAttacks()
     },
     specialAttack() {
-      this.monsterHealth -= this.calculateDamage(10, 20)
+      const damage = this.calculateDamage(10, 20)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster hard for ${damage}`,
+      })
       if (this.checkWin()) {
         return
       }
@@ -31,14 +43,23 @@ new Vue({
       } else {
         this.playerHealth = 100
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster hard for ${damage}`,
+      })
       this.monsterAttacks()
     },
     giveUp() {
       this.gameIsRunning = false
     },
     monsterAttacks() {
-      this.playerHealth -= this.calculateDamage(5, 12)
+      const damage = this.calculateDamage(5, 12)
+      this.playerHealth -= damage
       this.checkWin()
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10',
+      })
     },
     calculateDamage(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min)
